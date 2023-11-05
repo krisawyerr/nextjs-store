cf4794fec9774a06930dd006f6d755a3ee5f8eb6
krisawyerr <krisawyerrr@gmail.com>
2023-11-04 19:09:31 -0700

    first commit

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Flair",
+  description: "Expertly designed goods",
+  icon: "/favicon.ico",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+
+}
